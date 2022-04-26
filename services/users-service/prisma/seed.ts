fcab136536b4adb523f4727fd80ae3fb1482da0b
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-04-27 00:24:01 +0530

    prisma seeds added

---
+++ services/users-service/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.role.createMany({
+    data: [
+      {
+        name: 'admin',
+      },
+      {
+        name: 'customer',
+      },
+      {
+        name: 'hotel-admin',
+      },
+    ],
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
