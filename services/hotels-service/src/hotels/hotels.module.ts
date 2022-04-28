540930c1d2c4a6efe5314e86d4e4f026664df10c
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-04-28 17:12:31 +0530

    hotels controller

--- services/hotels-service/src/hotels/hotels.module.ts
+++ services/hotels-service/src/hotels/hotels.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HotelsService } from './hotels.service';
 import { HotelsController } from './hotels.controller';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
+  imports: [SharedModule],
   providers: [HotelsService],
   controllers: [HotelsController],
 })
