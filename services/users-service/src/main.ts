b32dda040dd75ea2af9f04d8a66c28a8523f07d9
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-04-28 10:02:20 +0530

    prisma moved

--- services/users-service/src/main.ts
+++ services/users-service/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './shared/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(4001);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 }
 bootstrap();
