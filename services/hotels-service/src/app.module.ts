ba0d221a2791647d12d2bf06b70d68ec47dc66c1
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-04-28 16:19:18 +0530

    hotels module

--- services/hotels-service/src/app.module.ts
+++ services/hotels-service/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
+import { HotelsModule } from './hotels/hotels.module';
 
 @Module({
-  imports: [SharedModule]
+  imports: [SharedModule, HotelsModule],
 })
 export class AppModule {}
