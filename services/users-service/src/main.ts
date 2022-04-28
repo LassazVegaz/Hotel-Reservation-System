3ad59bfda012df43a516f9d50a1a581a8d9d11d6
Lasindu Weerasinghe <lasindunuwanga@gmail.com>
2022-04-28 22:42:49 +0530

    port moved to .env

--- services/users-service/src/main.ts
+++ services/users-service/src/main.ts
@@ -4,7 +4,7 @@ import { PrismaService } from './shared/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4001);
+  await app.listen(process.env.PORT);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
