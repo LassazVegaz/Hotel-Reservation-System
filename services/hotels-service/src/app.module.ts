6408b9307a9a0cc515d0ce34a6ce28ed530f1f82
LassazVegaz <lasindunuwanga@gmail.com>
2022-05-04 22:33:08 +0530

    hotel reservations

--- services/hotels-service/src/app.module.ts
+++ services/hotels-service/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
 import { HotelsModule } from './hotels/hotels.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [SharedModule, HotelsModule],
+  imports: [SharedModule, HotelsModule, ReservationsModule],
 })
 export class AppModule {}
---
+++ services/hotels-service/src/reservations/reservations.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Reservation } from '@prisma/client';
+import { ReservationsService } from './reservations.service';
+
+@Controller(':hotelId/reservations')
+export class ReservationsController {
+  constructor(private readonly reservationsService: ReservationsService) {}
+
+  @Get()
+  async getReservations(
+    @Param('hotelId') hotelId: string,
+  ): Promise<Reservation[]> {
+    return this.reservationsService.getReservations(parseInt(hotelId));
+  }
+
+  @Get(':id')
+  async getReservation(@Param('id') id: string): Promise<Reservation> {
+    return this.reservationsService.getReservation(parseInt(id));
+  }
+
+  @Post()
+  async createReservation(
+    @Param('hotelId') hotelId: string,
+    @Body() reservation: Reservation,
+  ): Promise<Reservation> {
+    reservation.hotelId = parseInt(hotelId);
+    return this.reservationsService.createReservation(reservation);
+  }
+
+  @Patch(':id')
+  async updateReservation(
+    @Param('hotelId') hotelId: string,
+    @Param('id') id: string,
+    @Body() reservation: Reservation,
+  ): Promise<Reservation> {
+    reservation.hotelId = parseInt(hotelId);
+    return this.reservationsService.updateReservation(
+      parseInt(id),
+      reservation,
+    );
+  }
+
+  @Delete(':id')
+  async deleteReservation(@Param('id') id: string): Promise<void> {
+    await this.reservationsService.deleteReservation(parseInt(id));
+  }
+}
