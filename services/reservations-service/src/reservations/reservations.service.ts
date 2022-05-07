14dff8d4423e38925624c51521ba40933b2da53d
LassazVegaz <lasindunuwanga@gmail.com>
2022-05-07 20:03:52 +0530

    reservations finished

--- services/reservations-service/src/reservations/reservations.service.ts
+++ services/reservations-service/src/reservations/reservations.service.ts
@@ -1,4 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Reservation } from '@prisma/client';
+import { PrismaService } from 'src/shared/prisma.service';
 
 @Injectable()
-export class ReservationsService {}
+export class ReservationsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createReservation(reservation: Reservation): Promise<Reservation> {
+    // remove ID
+    delete reservation.id;
+
+    return this.prismaService.reservation.create({
+      data: reservation,
+    });
+  }
+
+  async getReservations(customerId: number): Promise<Reservation[]> {
+    return this.prismaService.reservation.findMany({
+      where: {
+        customerId,
+      },
+    });
+  }
+
+  async getReservation(id: number): Promise<Reservation> {
+    return this.prismaService.reservation.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updateReservation(
+    id: number,
+    reservation: Reservation,
+  ): Promise<Reservation> {
+    // check if reservation exists
+    if (!(await this.reservationExists(id)))
+      throw new NotFoundException(`Reservation with ID ${id} not found`);
+
+    // on date range can be updated
+    const { id: _, customerId, reservationId, ...data } = reservation;
+
+    return this.prismaService.reservation.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async deleteReservation(id: number): Promise<void> {
+    // check if reservation exists
+    if (!(await this.reservationExists(id)))
+      throw new NotFoundException(`Reservation with ID ${id} not found`);
+
+    await this.prismaService.reservation.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  private async reservationExists(id: number): Promise<boolean> {
+    const reservation = await this.prismaService.reservation.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return !!reservation;
+  }
+}
