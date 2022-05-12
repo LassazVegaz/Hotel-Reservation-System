35ae6870f983fff6fd3a422cb4ab35d7c6c19c86
LassazVegaz <lasindunuwanga@gmail.com>
2022-05-12 17:16:45 +0530

    create acc page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
+import { CreateAccountPage } from "./pages/CreateAccountPage/CreateAccountPage";
 
 function App() {
 	return (
 		<div>
 			<Header />
+
+			<CreateAccountPage />
 
 			<Footer />
 		</div>
