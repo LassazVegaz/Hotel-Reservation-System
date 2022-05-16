56f86de4a7b483d5fca7734270159e76fda5470c
LassazVegaz <lasindunuwanga@gmail.com>
2022-05-16 15:51:45 +0530

    create user API helper func

---
+++ frontend/src/helpers/api.helpers.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { User } from "../types/user.type";
+import config from "../config.json";
+
+const _axios = axios.create({
+	baseURL: config.baseUrl,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+
+const createUser = async (user: User) => {
+	try {
+		const res = await _axios.post<User>("/users", user);
+		return res.data;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const apiHelpers = {
+	createUser,
+};
