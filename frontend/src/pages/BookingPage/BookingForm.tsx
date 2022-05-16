26cf56318988d8b10e1e9fc667d5dfeb46cc6e73
LassazVegaz <lasindunuwanga@gmail.com>
2022-05-16 09:12:24 +0530

    booking form buttons

--- frontend/src/pages/BookingPage/BookingForm.tsx
+++ frontend/src/pages/BookingPage/BookingForm.tsx
@@ -1,5 +1,13 @@
 import styled from "@emotion/styled";
-import { TextField, Box, Card, CardContent } from "@mui/material";
+import {
+	TextField,
+	Box,
+	Card,
+	CardContent,
+	Typography,
+	CardActions,
+	Button,
+} from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 import moment from "moment";
 import { useState } from "react";
@@ -22,32 +30,60 @@ export const BookingForm = () => {
 	return (
 		<Box component="form" mt={5}>
 			<Card>
-				<CardContent
+				<CardContent>
+					<Box
+						display="flex"
+						justifyContent="space-evenly"
+						columnGap={5}
+					>
+						<DatePicker
+							inputFormat="DD/MM/YYYY"
+							label="From"
+							minDate={moment()}
+							value={from.fromDate}
+							onChange={(date) =>
+								setFrom({ ...from, fromDate: date })
+							}
+							renderInput={(props) => <DateControl {...props} />}
+						/>
+						<DatePicker
+							inputFormat="DD/MM/YYYY"
+							label="To"
+							minDate={moment()}
+							value={from.toDate}
+							onChange={(date) =>
+								setFrom({ ...from, toDate: date })
+							}
+							renderInput={(props) => <DateControl {...props} />}
+						/>
+					</Box>
+
+					<Box mt={5} display="flex">
+						<Typography>Price:</Typography>
+						<Typography ml={3} fontWeight="bold">
+							500.00
+						</Typography>
+					</Box>
+				</CardContent>
+
+				<CardActions
 					sx={{
-						display: "flex",
-						justifyContent: "space-evenly",
-						columnGap: 5,
+						justifyContent: "flex-end",
 					}}
 				>
-					<DatePicker
-						inputFormat="DD/MM/YYYY"
-						label="From"
-						minDate={moment()}
-						value={from.fromDate}
-						onChange={(date) =>
-							setFrom({ ...from, fromDate: date })
-						}
-						renderInput={(props) => <DateControl {...props} />}
-					/>
-					<DatePicker
-						inputFormat="DD/MM/YYYY"
-						label="To"
-						minDate={moment()}
-						value={from.toDate}
-						onChange={(date) => setFrom({ ...from, toDate: date })}
-						renderInput={(props) => <DateControl {...props} />}
-					/>
-				</CardContent>
+					<Button
+						variant="outlined"
+						color="secondary"
+						sx={{
+							mr: 2,
+						}}
+					>
+						Cancel
+					</Button>
+					<Button variant="contained" color="primary">
+						Book
+					</Button>
+				</CardActions>
 			</Card>
 		</Box>
 	);
