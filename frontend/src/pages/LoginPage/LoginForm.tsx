abbd30fc967e744f5be1ddec22dff3b6d74d5424
LassazVegaz <lasindunuwanga@gmail.com>
2022-05-17 08:03:11 +0530

    login form validation

--- frontend/src/pages/LoginPage/LoginForm.tsx
+++ frontend/src/pages/LoginPage/LoginForm.tsx
@@ -1,38 +1,61 @@
-import { Box, TextField, Button } from "@mui/material";
-import { Formik } from "formik";
+import { Box, Button } from "@mui/material";
+import { useFormik } from "formik";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { FormikMUITextField } from "../../components/FormikMUITextField/FormikMUITextField";
+
+const validationSchema = Yup.object({
+	email: Yup.string().email("Invalid email").required("Required"),
+	password: Yup.string().required("Required"),
+});
+
+const initialValues = {
+	email: "",
+	password: "",
+};
 
 export const LoginForm = () => {
 	const navigate = useNavigate();
+	const form = useFormik({
+		initialValues,
+		onSubmit: (values) => {
+			console.log(values);
+		},
+		validationSchema,
+	});
 
 	return (
-		<Formik
-			initialValues={{}}
-			onSubmit={() => {
-				console.log("creating");
-			}}
+		<Box
+			component="form"
+			display="flex"
+			flexDirection="column"
+			rowGap={3}
+			onSubmit={form.handleSubmit}
 		>
-			<Box
-				component="form"
-				display="flex"
-				flexDirection="column"
-				rowGap={3}
-			>
-				<TextField label="Email" type="email" />
-				<TextField label="Password" type="password" />
+			<FormikMUITextField label="Email" name="email" form={form} />
+			<FormikMUITextField
+				label="Password"
+				name="password"
+				form={form}
+				type="password"
+			/>
 
-				<Button
-					sx={{
-						mt: 3,
-					}}
-					variant="contained"
-				>
-					Sign In
-				</Button>
-				<Button variant="contained" onClick={() => navigate("/")}>
-					Sign Up
-				</Button>
-			</Box>
-		</Formik>
+			<Button
+				sx={{
+					mt: 3,
+				}}
+				variant="contained"
+				type="submit"
+			>
+				Sign In
+			</Button>
+			<Button
+				color="secondary"
+				variant="contained"
+				onClick={() => navigate("/")}
+			>
+				Sign Up
+			</Button>
+		</Box>
 	);
 };
