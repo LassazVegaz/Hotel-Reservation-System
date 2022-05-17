25424d781a303d6ddde1f898731366896210573b
LassazVegaz <lasindunuwanga@gmail.com>
2022-05-17 08:11:05 +0530

    new api endpoints json

--- frontend/src/helpers/users-api.helpers.ts
+++ frontend/src/helpers/users-api.helpers.ts
@@ -1,9 +1,10 @@
 import { User } from "../types/user.type";
 import { appAxios } from "./api.helpers";
+import endpoints from "../api-endpoints.json";
 
 const createUser = async (user: User) => {
 	try {
-		const res = await appAxios.post<User>("/users", user);
+		const res = await appAxios.post<User>(endpoints.users.common, user);
 		return res.data;
 	} catch (error) {
 		console.error(error);
