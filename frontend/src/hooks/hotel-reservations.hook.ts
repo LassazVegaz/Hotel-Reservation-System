3a346b4536d2cc37f616aacf7dfa873a6c0d28d7
LassazVegaz <lasindunuwanga@gmail.com>
2022-05-19 12:58:06 +0530

    display hotel in booking page

--- frontend/src/hooks/hotel-reservations.hook.ts
+++ frontend/src/hooks/hotel-reservations.hook.ts
@@ -1,5 +1,7 @@
 import { reservationsApiHelper } from "../helpers/hotel-reservations-api.helper";
+import { hotelsApiHelper } from "../helpers/hotels-api.helper";
 import { Reservation } from "../types/hotel-reservation.type";
+import { Hotel } from "../types/hotel.type";
 import { useApi } from "./api.hook";
 
 export const useReservationsApi = () => {
@@ -41,8 +43,39 @@ export const useReservationsApi = () => {
 		return reservations;
 	};
 
+	const getReservationAndHotel = async (
+		reservationId: number
+	): Promise<{
+		reservation: Reservation;
+		hotel: Hotel;
+	} | null> => {
+		let reservation: Reservation | null = null;
+		let hotel: Hotel | null = null;
+
+		await api(
+			async () => {
+				reservation = await reservationsApiHelper.getReservation(
+					reservationId
+				);
+				hotel = await hotelsApiHelper.getHotelById(reservation.hotelId);
+			},
+			{
+				showErrorNotification: false,
+				showSuccessNotification: false,
+			}
+		);
+
+		return reservation && hotel
+			? {
+					reservation,
+					hotel,
+			  }
+			: null;
+	};
+
 	return {
 		createReservation,
 		getReservations,
+		getReservationAndHotel,
 	};
 };
